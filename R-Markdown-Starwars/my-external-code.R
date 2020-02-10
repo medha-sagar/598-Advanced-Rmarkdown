7ea0beca4f29b783b9dbd258897f5772367787b8
Medha Sagar <medhasagar94@gmail.com>
2020-02-10 11:47:50 -0800

    Initial Commit

---
+++ R-Markdown-Starwars/my-external-code.R
@@ -0,0 +1,14 @@
+# Here's my R script file, 
+# in the same directory as 
+# my Rmd file 
+
+# ---- data-intro
+
+  rows = dim(starwars)[1]
+  cols = dim(starwars)[2]
+  print(paste(rows,"rows and",cols, "columns"))
+
+  
+
+  
+  
